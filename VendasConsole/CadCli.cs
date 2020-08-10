e276fb4648edf7ee2c729008452c90f5fb58ed42
ramonkj <rk0191@hotmail.com>
2020-08-10 20:30:26 -0300

    Criação do menu inicial

---
+++ VendasConsole/CadCli.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VendasConsole
+{
+    class CadCli
+    {
+    }
+}
--- VendasConsole/Program.cs
+++ VendasConsole/Program.cs
@@ -6,7 +6,46 @@ namespace VendasConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int op;
+            Console.WriteLine("[]======================================[]");
+            Console.WriteLine("           Sistema de Vendas");
+            Console.WriteLine("[]======================================[]\n");
+            Console.WriteLine("Menu");
+            Console.WriteLine(" 1- Cadastrar Cliente\n 2- Listar Clientes\n 3- Cadastrar Vendedor\n 4- Listar Vendedores\n 5- Cadastrar Produto\n 6- Listar Produtos\n 7- Registrar Venda\n 8- Listar Vendas\n 9- Listar Vendas por Cliente\n 0- Sair\n");
+            Console.Write("Selecione uma opção: ");
+            op = Convert.ToInt32(Console.ReadLine());
+            switch (op) {
+                case 1:
+                    Console.WriteLine("\n[]-- Cadastro de clientes --[]");
+                    break;
+                case 2:
+                    Console.WriteLine("\n[]-- Listagem de clientes --[]");
+                    break;
+                case 3:
+                    Console.WriteLine("\n[]-- Cadastro de vendedores --[]");
+                    break;
+                case 4:
+                    Console.WriteLine("\n[]-- Listagem de vendedores --[]");
+                    break;
+                case 5:
+                    Console.WriteLine("\n[]-- Cadastro de produtos --[]");
+                    break;
+                case 6:
+                    Console.WriteLine("\n[]-- Listagem de produtos --[]");
+                    break;
+                case 7:
+                    Console.WriteLine("\n[]-- Registro de venda --[]");
+                    break;
+                case 8:
+                    Console.WriteLine("\n[]-- Listagem de vendas --[]");
+                    break;
+                case 9:
+                    Console.WriteLine("\n[]-- Listagem de vendas por cliente --[]");
+                    break;
+                case 0:
+                    Console.WriteLine("\n[]-- Saindo...");
+                    break;
+            }
         }
     }
 }
